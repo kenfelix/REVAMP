720052065de75fbaf34442aa77a2d0e1e0ae98bd
kenfelix <Kmrapper.kf@gmail.com>
2023-01-04 12:03:59 +0100

    updates

--- app/utils/scraper.py
+++ app/utils/scraper.py
@@ -30,7 +30,7 @@ class Scraper:
         if self.driver is None:
             user_agent = get_user_agent()
             options = Options()
-            options.binary_location = "/usr/bin/google-chrome-stable"
+            options.binary_location = "/usr/bin/google-chrome"
             options.add_argument("--no-sandbox")
             options.add_argument("--headless")
             options.add_argument("--disable-gpu")
