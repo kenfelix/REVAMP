a1a648389a46c3574f610af3c42d435fd810ef43
kenfelix <Kmrapper.kf@gmail.com>
2023-01-04 17:20:04 +0100

    updates

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from app.engine import s, scrape_links
 
 
 def start_engine():
-    login = s.linkedin_login(email=settings.linkedin_email, password=settings.linkedin_password)
+    # login = s.linkedin_login(email=settings.linkedin_email, password=settings.linkedin_password)
     # if login is False:
     #     s.close_driver()
     #     start_engine()
-    # scrape_links.delay("graphics")
+    scrape_links.delay("graphics")
 
 
 if __name__ == "__main__":
