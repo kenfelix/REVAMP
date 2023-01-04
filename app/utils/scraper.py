0f86ce6bb75117fbd7a58cfabcef117b7c699346
kenfelix <Kmrapper.kf@gmail.com>
2023-01-04 12:03:25 +0100

    updates

--- app/utils/scraper.py
+++ app/utils/scraper.py
@@ -34,6 +34,7 @@ class Scraper:
             options.add_argument("--no-sandbox")
             options.add_argument("--headless")
             options.add_argument("--disable-gpu")
+            options.add_argument('--disable-dev-shm-usage')
             options.add_argument(f"user-agent={user_agent}")
             # options.add_argument('--start-maximized')
             options.add_experimental_option("excludeSwitches", ["enable-logging"])
