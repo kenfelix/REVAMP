ff2c8f46c18c6157e7ae5d2899f346bf26da5d77
kenfelix <Kmrapper.kf@gmail.com>
2023-01-04 10:54:59 +0100

    updates

--- app/config/config.py
+++ app/config/config.py
@@ -4,7 +4,6 @@ from pydantic import BaseSettings, EmailStr
 class Settings(BaseSettings):
     linkedin_email: EmailStr
     linkedin_password: str
-    redirect_url: str
     linkedin_home_url: str
     linkedin_login_url: str
     rabbitmq_username: str
