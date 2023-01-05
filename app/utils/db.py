fd668b8ddfc690da761d1e89837a933ae013c91c
kenfelix <Kmrapper.kf@gmail.com>
2023-01-05 14:58:17 +0100

    updates

--- app/utils/db.py
+++ app/utils/db.py
@@ -11,13 +11,16 @@ def insert_job(job: dict):
         job_Indb = db.english.find_one({"job title": title, "experience level": rate})
         if job_Indb:
             skill_Indb = job_Indb["required skills"]
-            skills.extend(skill_Indb)
-            db.english.insert_one(
-                {
-                    "job title": title,
-                    "experience level": rate,
-                    "required skills": skills,
-                }
+            skills = list(set(skills.extend(skill_Indb)))
+            db.english.update_one(
+                filter={"_id": job_Indb.get("_id")},
+                update={
+                    "$set": {
+                        "job title": title,
+                        "experience level": rate,
+                        "required skills": skills,
+                    }
+                },
             )
         else:
             db.english.insert_one(job)
